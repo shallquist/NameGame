2c9af40690a1da5b0de89f23cd9a7f7b3811ce9e
steig hallquist <shallquist@optimo-it.com>
2017-08-31 11:04:02 -0400

    Initial Commit

---
+++ NameGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NameGame
+//
+//  Created by steig hallquist on 8/31/17.
+//  Copyright © 2017 steig hallquist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
